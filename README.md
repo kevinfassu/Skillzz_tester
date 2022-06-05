0eaa30a7c37824bb744ac214982aed4a5aacfffb
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-05 00:47:15 -0700

    completed work

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Skillzz_tester
+## Skillzz_tester
+
+# Descrition
+Created a mini quiz for beginner to test their knowledge on web development conecpts
+
+## Installtion 
+Pages: https://kevinfassu.github.io/Skillzz_tester/
+github: https://github.com/kevinfassu/Skillzz_tester
+
+# Citations 
---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does .append do",
+    answer: "insersts content at end of existing element",
+    options: [
+      "Nothing",
+      "insersts content at end of existing element",
+      "it is how to declare a variable",
+      "logs data to console"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many licks does it take to get to the center of a tootsie pop",
+    answer: "1000",
+    options: [
+      "100000",
+      "1000",
+      "2000",
+      "50000"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does CSS do",
+    answer: "Adds Style",
+    options: [
+      "Adds Style",
+      "Nothing",
+      "Adds fuctionality",
+      "None of the above"
+    ]
+  },
+];
